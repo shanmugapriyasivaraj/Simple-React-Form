1593d8cbd736ed63d23fd2b13afeecc9d43d807b
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-04-24 20:45:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Form from "../src/Components/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
